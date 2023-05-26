10dd87eeb05132dd123f5288bbd295c56dc65eee
Altuggurgul <84250064+Altuggurgul@users.noreply.github.com>
2023-05-26 15:36:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ if authentication_status:
     def show_pdf(file_path):
         with open(file_path, "rb") as f:
             base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="1280" height="720" type="application/pdf">'
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1280" height="720" type="application/pdf"></iframe>'
         st.markdown(pdf_display, unsafe_allow_html=True)
 
 
